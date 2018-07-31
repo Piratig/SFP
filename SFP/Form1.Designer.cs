4044bccffdc8f6fa53df93c20438592faad45f17
Kharkovskiy IS <his1994@mail.ru>
2018-07-31 15:27:19 +0300

    Initial commit

---
+++ SFP/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace SFP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStartDirectory = new System.Windows.Forms.Button();
+            this.textBoxStartDirectory = new System.Windows.Forms.TextBox();
+            this.textBoxFileNameTemplate = new System.Windows.Forms.TextBox();
+            this.textBoxText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.richTextBoxOutPut = new System.Windows.Forms.RichTextBox();
+            this.textBoxActiveFile = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonStartDirectory
+            // 
+            this.buttonStartDirectory.Location = new System.Drawing.Point(649, 21);
+            this.buttonStartDirectory.Name = "buttonStartDirectory";
+            this.buttonStartDirectory.Size = new System.Drawing.Size(139, 23);
+            this.buttonStartDirectory.TabIndex = 0;
+            this.buttonStartDirectory.Text = "Стартовая директория";
+            this.buttonStartDirectory.UseVisualStyleBackColor = true;
+            this.buttonStartDirectory.Click += new System.EventHandler(this.buttonStartDirectory_Click);
+            // 
+            // textBoxStartDirectory
+            // 
+            this.textBoxStartDirectory.Location = new System.Drawing.Point(191, 23);
+            this.textBoxStartDirectory.Name = "textBoxStartDirectory";
+            this.textBoxStartDirectory.Size = new System.Drawing.Size(437, 20);
+            this.textBoxStartDirectory.TabIndex = 1;
+            // 
+            // textBoxFileNameTemplate
+            // 
+            this.textBoxFileNameTemplate.Location = new System.Drawing.Point(191, 63);
+            this.textBoxFileNameTemplate.Name = "textBoxFileNameTemplate";
+            this.textBoxFileNameTemplate.Size = new System.Drawing.Size(437, 20);
+            this.textBoxFileNameTemplate.TabIndex = 2;
+            // 
+            // textBoxText
+            // 
+            this.textBoxText.Location = new System.Drawing.Point(191, 106);
+            this.textBoxText.Name = "textBoxText";
+            this.textBoxText.Size = new System.Drawing.Size(437, 20);
+            this.textBoxText.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Стартовая директория";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Шаблон имени файла";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Текст, содержащийся в файле";
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(649, 104);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(139, 23);
+            this.buttonSearch.TabIndex = 7;
+            this.buttonSearch.Text = "Поиск";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // richTextBoxOutPut
+            // 
+            this.richTextBoxOutPut.Location = new System.Drawing.Point(415, 145);
+            this.richTextBoxOutPut.Name = "richTextBoxOutPut";
+            this.richTextBoxOutPut.Size = new System.Drawing.Size(373, 293);
+            this.richTextBoxOutPut.TabIndex = 8;
+            this.richTextBoxOutPut.Text = "";
+            // 
+            // textBoxActiveFile
+            // 
+            this.textBoxActiveFile.Location = new System.Drawing.Point(191, 145);
+            this.textBoxActiveFile.Name = "textBoxActiveFile";
+            this.textBoxActiveFile.Size = new System.Drawing.Size(207, 20);
+            this.textBoxActiveFile.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxActiveFile);
+            this.Controls.Add(this.richTextBoxOutPut);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxText);
+            this.Controls.Add(this.textBoxFileNameTemplate);
+            this.Controls.Add(this.textBoxStartDirectory);
+            this.Controls.Add(this.buttonStartDirectory);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStartDirectory;
+        private System.Windows.Forms.TextBox textBoxStartDirectory;
+        private System.Windows.Forms.TextBox textBoxFileNameTemplate;
+        private System.Windows.Forms.TextBox textBoxText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.RichTextBox richTextBoxOutPut;
+        private System.Windows.Forms.TextBox textBoxActiveFile;
+    }
+}
+
---
+++ SFP/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace SFP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStartDirectory_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbdStartDirectory = new FolderBrowserDialog();
+            if (fbdStartDirectory.ShowDialog() == DialogResult.OK)
+            {
+                textBoxStartDirectory.Text = fbdStartDirectory.SelectedPath;
+            }
+        }
+
+        private void Seasrh(string directory, string content, string fileNameSearch)
+        {
+            DirectoryInfo di = new DirectoryInfo(@directory);
+
+            FileInfo[] fileArray = di.GetFiles();
+
+            string[] diNames = Directory.GetFiles(@directory, fileNameSearch);
+
+            foreach (string diName in diNames)
+            {
+                //MessageBox.Show(diName);
+                textBoxActiveFile.Text = diName;
+                string[] text = File.ReadAllLines(diName);
+                for (int i = 0; i < text.Length; i++)
+                {
+                    //sLine = 
+                    if (text[i] == content)
+                    {
+                        richTextBoxOutPut.Text += diName + "\n";
+                        richTextBoxOutPut.Text += text[i] + "\n";
+                        break;
+                    }
+                }
+                //text.Close();
+                //textBoxActiveFile.Text = "";
+            }
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            string fileNameSearch = "*";
+            if (textBoxFileNameTemplate.Text == null)
+            {
+                fileNameSearch = textBoxFileNameTemplate.Text;
+            }
+            Seasrh(textBoxStartDirectory.Text, textBoxText.Text, fileNameSearch);
+        }
+    }
+}
