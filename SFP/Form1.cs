07e83135cb48df5b90e8c9903cee7982b3358ff4
Kharkovskiy IS <his1994@mail.ru>
2018-08-01 12:37:47 +0300

    45%

--- SFP/Form1.cs
+++ SFP/Form1.cs
@@ -34,63 +34,33 @@ namespace SFP
 
         private void Seasrh(string directory, string content, string fileNameSearch)
         {
-            //DirectoryInfo di = new DirectoryInfo(@directory);
-
-            //FileInfo[] fileArray = di.GetFiles();
-
-            //string[] diNames = Directory.GetFiles(@directory, fileNameSearch);
-
-            //foreach (string diName in diNames)
-            //{
-            //    textBoxActiveFile.Text = diName;
-            //    string[] text = File.ReadAllLines(diName);
-            //    for (int i = 0; i < text.Length; i++)
-            //    {
-            //        bool b = text[i].Contains(content);
-            //        if (b)
-            //        {
-            //            richTextBoxOutPut.Text += diName + "\n";
-            //            richTextBoxOutPut.Text += text[i] + "\n";
-            //            break;
-            //        }
-            //    }
-            //}
-
             string[] drives = Environment.GetLogicalDrives();
 
             foreach (string dr in drives)
             {
                 DriveInfo di = new DriveInfo(dr);
 
-                // Here we skip the drive if it is not ready to be read. This
-                // is not necessarily the appropriate action in all scenarios.
                 if (!di.IsReady)
                 {
-                    //Console.WriteLine("The drive {0} could not be read", di.Name);
                     continue;
                 }
                 DirectoryInfo rootDir = new DirectoryInfo(@directory);
-                WalkDirectoryTree(rootDir, fileNameSearch);
+                WalkDirectoryTree(rootDir, fileNameSearch, content);
+                break;
             }
         }
 
-        private void WalkDirectoryTree(DirectoryInfo root, string fileNameSearch)
+        private void WalkDirectoryTree(DirectoryInfo root, string fileNameSearch, string content)
         {
             FileInfo[] files = null;
             DirectoryInfo[] subDirs = null;
 
-            // First, process all the files directly under this folder
             try
             {
                 files = root.GetFiles(fileNameSearch);
             }
-            // This is thrown if even one of the files requires permissions greater
-            // than the application provides.
             catch (UnauthorizedAccessException e)
             {
-                // This code just writes out the message and continues to recurse.
-                // You may decide to do something different here. For example, you
-                // can try to elevate your privileges and access the file again.
                 log.Add(e.Message);
             }
 
@@ -103,32 +73,38 @@ namespace SFP
             {
                 foreach (FileInfo fi in files)
                 {
-                    // In this example, we only access the existing FileInfo object. If we
-                    // want to open, delete or modify the file, then
-                    // a try-catch block is required here to handle the case
-                    // where the file has been deleted since the call to TraverseTree().
-                    richTextBoxOutPut.Text += fi.FullName + "\n";
+                    textBoxActiveFile.Text = fi.FullName;
+                    string[] text = File.ReadAllLines(fi.FullName);
+                    for (int i = 0; i < text.Length; i++)
+                    {
+                        bool b = text[i].Contains(content);
+                        if (b)
+                        {
+                            richTextBoxOutPut.Text += fi.FullName + "\n";
+                            break;
+                        }
+                    }
                 }
 
-                // Now find all the subdirectories under this directory.
                 subDirs = root.GetDirectories();
 
                 foreach (DirectoryInfo dirInfo in subDirs)
                 {
-                    // Resursive call for each subdirectory.
-                    WalkDirectoryTree(dirInfo, fileNameSearch);
+                    WalkDirectoryTree(dirInfo, fileNameSearch, content);
                 }
             }
         }
 
         private void buttonSearch_Click(object sender, EventArgs e)
         {
+            richTextBoxOutPut.Clear();
             string fileNameSearch = "*";
-            if (textBoxFileNameTemplate.Text == null)
+            if (textBoxFileNameTemplate.Text != "")
             {
                 fileNameSearch = textBoxFileNameTemplate.Text;
             }
             Seasrh(textBoxStartDirectory.Text, textBoxText.Text, fileNameSearch);
+            
         }
     }
 }
